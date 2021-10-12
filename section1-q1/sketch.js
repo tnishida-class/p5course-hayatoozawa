1bd8965c06ae6ef4ce4bd035b38ecba58c097f4a
hayatoozawa <92037059+hayatoozawa@users.noreply.github.com>
2021-10-12 13:24:14 +0900

    Update sketch.js
    make Kobe-shi's logo

--- section1-q1/sketch.js
+++ section1-q1/sketch.js
@@ -5,9 +5,13 @@ function setup(){
 
   // 空欄を埋めて神戸市のロゴマークを完成させよう
   noFill();
+  stroke(0,128,0);
   strokeWeight(25);
   strokeCap(SQUARE);
-  arc(100 + 25, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(100 + 20, 100, 100, 100, QUARTER_PI * 3, QUARTER_PI * 3 + PI);
+  arc(70, 100, 100, 100, QUARTER_PI * 5, QUARTER_PI);
+
+
   // BLANK[1]
 
   // おまけ：神戸大学のロゴを作りたい人向けに色情報だけ置いておきます
