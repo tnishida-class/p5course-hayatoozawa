4fb24545b3f6506d35ba70e8206d2a7788b6ce64
hayatoozawa <92037059+hayatoozawa@users.noreply.github.com>
2021-10-24 23:58:50 +0900

    2-q3

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -11,25 +11,32 @@ function setup() {
   strokeWeight(3);
 
   const cx = width / 2; // 中心は (cx, cy)
-  const cy = height / 2;
+  const cy = width / 2;
   const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
-
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
+  drawArcs(cream, black, maxR* 0.75);
+  drawArcs(green, red, maxR * 0.5);
+  drawArcs(cream, black, maxR* 0.45);
+  drawCircle(green, maxR * 0.09);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
   drawCircle(red, maxR * 0.05);
 }
 
 function drawCircle(c, r){
+  const cx = width / 2; // 中心は (cx, cy)
+  const cy = width / 2;
   fill(c);
   ellipse(cx, cy, r, r);
 }
 
 function drawArcs(c1, c2, r) {
+  const cx = width / 2; // 中心は (cx, cy)
+  const cy = width / 2;
   for (let i = 0; i < 20; i++) {
     let start = TWO_PI / 20 * i;
     let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
+    fill(i % 2 == 0 ? c1:c2);
     arc(cx, cy, r, r, start, stop, PIE);
   }
 }
