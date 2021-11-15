d999f29269fdd00c73287d02b6e77af7a4cb4a25
hayatoozawa <92037059+hayatoozawa@users.noreply.github.com>
2021-11-15 20:12:01 +0900

    section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,27 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love you", 200, 200, 45, 100, 14);
+  balloon("No pain, no gain", 300, 100, 10, 100, 100);
 }
 
-function balloon(t){
-  let w = textWidth(t);
-  let h = textAscent() + textDescent();
+function balloon(t, x, y, c, d, e){
   let p = 2;
+  let w = textWidth(t);
+  let h = textDescent() - textAscent();
+  let r = w + p;
+  let a = - r / 2 + x;
+  let b = - h / 2 + y;
+  let z = 10
+  let theta = TWO_PI / 6
+  fill(c, d, e);
+  ellipse(x, y, r);
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  text(t, a, b);
+  beginShape();
+  vertex(x, r / 2 + y);
+  vertex(x - z, y + r + z);
+  
+  endShape();
 }
+console.log();
